47bb79404044ec1c19c901c2276e8dffd3bb1c8d
thang0804 <linhmonmon2201@gmail.com>
2020-05-02 07:17:36 +0700

    calclib

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calclib
+### Thư viện tính toán cho python. Giúp các phép tính đi xa hơn, dễ sử dụng hơn
---
+++ calclib/Lexer.py
@@ -0,0 +1,37 @@
+import re
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.tokens = []
+    def GetTokens(self):
+        for tok in self.text:
+            if re.match('[0-9]', tok):
+                try:
+                    if self.tokens[-1][0] == 'INT' or self.tokens[-1][0] == 'FLOAT':
+                        self.tokens[-1][-1] += tok
+                    else:
+                        self.tokens.append(['INT', tok])
+                except:
+                    self.tokens.append(['INT', tok])
+            elif tok == '.':
+                try:
+                    if self.tokens[-1][0] == 'INT':
+                        self.tokens[-1][0] = 'FLOAT'
+                        self.tokens[-1][-1] += tok
+                    elif self.token[-1][0] == 'FLOAT':
+                        # Raise some error
+                        pass
+                    else:
+                        self.tokens.append(['FLOAT', '0.'])
+                except:
+                    self.tokens.append(['FLOAT', '0.'])
+            elif tok in '+-*/%':
+                self.tokens.append(['OP', tok])
+            elif tok == '(':
+                self.tokens.append(['LPARENT', tok])
+            elif tok == ')':
+                self.tokens.append(['RPARENT', tok])
+            elif tok == ' ':
+                self.tokens.append(['WHITESPACE'])
+        return self.tokens

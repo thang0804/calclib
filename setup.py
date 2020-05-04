419dc1f5503a8c56ecf61b6e05170e1afcc18f38
thang0804 <linhmonmon2201@gmail.com>
2020-05-04 14:58:34 +0700

    calclib

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="calclib",
+    version="0.0.1",
+    author="PyNone Team",
+    author_email="pydevmonkey4002@gmail.com",
+    description="calclib an calculator engine for GUI calculator",
+    url="https://github.com/thang0804/calclib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

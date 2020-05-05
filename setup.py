e45541e9252dfee106432cfd4d421abe96dd3ff7
thang0804 <linhmonmon2201@gmail.com>
2020-05-05 20:00:41 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 # Setup file for calclib
 setuptools.setup(
     name="calclib",
-    version="0.0.13",
+    version="0.0.14",
     author="PyNone Team",
     author_email="pydevmonkey4002@gmail.com",
     description="calclib an calculator engine for GUI or Console calculator",
